c286fcde912db6f0c410b37aec2deb6d08f68083
panyuerui <panyuerui@xinruan>
2015-12-31 11:19:09 +0800

    no message

--- RayBannerView/ViewController.swift
+++ RayBannerView/ViewController.swift
@@ -17,10 +17,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var imag: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-     //   bannerView.address = .Web(["http://img0.ph.126.net/SeZND7sJY2E9817aqyiF9Q==/6608636628980965995.jpg","http://img0.ph.126.net/Gqoi8jy24MIqewWmUNUVcA==/639792622080629406.jpg","http://img1.ph.126.net/tWT4rtsPXrsoPyVrjvP4uQ==/1120551882302603980.jpg"]);
+        //网络图片
+        bannerView.address = .Web(["http://img0.ph.126.net/SeZND7sJY2E9817aqyiF9Q==/6608636628980965995.jpg","http://img0.ph.126.net/Gqoi8jy24MIqewWmUNUVcA==/639792622080629406.jpg","http://img1.ph.126.net/tWT4rtsPXrsoPyVrjvP4uQ==/1120551882302603980.jpg"]);
+         //本地图片
         bannerView.address = .Local(["1","2","3"]);
+        //指示器颜色
         bannerView.currentColor = UIColor.blueColor();
-       // self.imag.sd_setImageWithURL(NSURL(string: "http://img0.ph.126.net/SeZND7sJY2E9817aqyiF9Q==/6608636628980965995.jpg"), placeholderImage: UIImage(named: "1"));
     }
 
     override func didReceiveMemoryWarning() {
